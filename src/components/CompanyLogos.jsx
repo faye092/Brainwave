1228ffa17af9912fb623de7075a52b56ecf3678c
faye092 <liaofei167@gmail.com>
2024-04-07 22:08:19 +0100

    update companylogos.jsx

---
+++ src/components/CompanyLogos.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CompanyLogos = ({ className }) => {
+  return (
+    <div className={className}>
+      <h5 className="tagline mb-6 text-center text-n-1/50">
+        Helping people create beautiful content at
+      </h5>
+      <ul></ul>
+    </div>
+  );
+};
+
+export default CompanyLogos;
