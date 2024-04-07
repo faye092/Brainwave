71946534780f367e5adb348b478f6b04e0c89f29
faye092 <liaofei167@gmail.com>
2024-04-07 15:08:40 +0100

    update the header, section and hero

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
 const Button = ({ className, href, onClick, children, px, white }) => {
-  const classes = `button relative infline-flex
+  const classes = `button relative inline-flex
                    items-center justify-center
                    h-11 transition-colors
                    hover:text-color-1 ${px || "px-7"}
@@ -11,7 +11,7 @@ const Button = ({ className, href, onClick, children, px, white }) => {
   const spanClasses = "relative z-10";
 
   const renderButton = () => (
-    <button className={classes}>
+    <button className={classes} onClick={onClick}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
