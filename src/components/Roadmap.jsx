b4df9dc8bd80eaabcb9c118a526e1b16c97f66a3
faye092 <liaofei167@gmail.com>
2024-04-25 16:07:04 +0100

    update Roadmap.jsx

---
+++ src/components/Roadmap.jsx
@@ -0,0 +1,14 @@
+import Section from "./Section";
+import Heading from "./Heading";
+
+const Roadmap = () => {
+  return (
+    <Section className="overflow-hidden" id="roadmap">
+      <div className="container md:pb-10">
+        <Heading tag="Ready to get started" title="What we're on" />
+      </div>
+    </Section>
+  );
+};
+
+export default Roadmap;
